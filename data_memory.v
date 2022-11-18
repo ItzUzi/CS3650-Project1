3f490a15de7526783b646944ba2be778b8072c05
Uziel Gaeta <ugaeta@cpp.edu>
2022-11-17 18:54:02 -0800

    added comments for understandability

--- data_memory.v
+++ data_memory.v
@@ -9,7 +9,7 @@
     output [15:0] mem_read_data  
  );  
       integer i;  
-      reg [15:0] ram [255:0];  
+      reg [15:0] ram [255:0];  // declares 256 element array at 16 bits wide each element
       wire [7 : 0] ram_addr = mem_access_addr[8 : 1];  
       initial begin  
            for(i=0;i<256;i=i+1)  
@@ -17,7 +17,7 @@
       end  
       always @(posedge clk) begin  // runs if clock changes val from 0 -> 1 or 1 -> 0
            if (mem_write_en)  
-                ram[ram_addr] <= mem_write_data;  // changes ram[ram_addr] to mem_write_data when clock == 1
+                ram[ram_addr] <= mem_write_data;  // changes ram[ram_addr] to mem_write_data when clock changes and mem_write_en == 1
       end  
-      assign mem_read_data = (mem_read==1'b1) ? ram[ram_addr]: 16'd0;   
+      assign mem_read_data = (mem_read==1'b1) ? ram[ram_addr]: 16'd0;   // Everytime mem_read changes, changes value of mem_read_data
  endmodule   
