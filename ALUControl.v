6f02f6a1a50107fbffda917ee2c1254daed95320
Michael Truong <truong.minh.michael@gmail.com>
2022-11-22 17:18:57 -0800

    more comments

--- ALUControl.v
+++ ALUControl.v
@@ -6,15 +6,15 @@ module ALUControl( ALU_Control, ALUOp, Function);
     assign ALUControlIn = {ALUOp,Function};  
     always @(ALUControlIn)  
     casex (ALUControlIn)  
-        6'b11xxxx: ALU_Control=3'b000;  
+        6'b11xxxx: ALU_Control=3'b000;  //if 2 most significant bits of ALUControlIn == 11, compute Addi instruction
         6'b10xxxx: ALU_Control=3'b100;  
         6'b01xxxx: ALU_Control=3'b001;  
-        6'b000000: ALU_Control=3'b000;  
-        6'b000001: ALU_Control=3'b001;  
+        6'b000000: ALU_Control=3'b000; 
+        6'b000001: ALU_Control=3'b001; 
         6'b000010: ALU_Control=3'b010;  
         6'b000011: ALU_Control=3'b011;  
         6'b000100: ALU_Control=3'b100;  
-        default: ALU_Control=3'b000;  
+        default: ALU_Control=3'b000;    
     endcase  
  endmodule  
 
