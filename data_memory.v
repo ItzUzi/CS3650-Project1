525d0139b5ea73e3c6cf0e2e3e702a3ba4a9588f
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2022-11-22 19:27:25 -0800

    Update data_memory.v
    switching to 32 bit

--- data_memory.v
+++ data_memory.v
@@ -1,24 +1,24 @@
  module data_memory  
  (  
     input clk,   //clock
-    input [15:0] mem_access_addr,  
-    input [15:0] mem_write_data,  
+    input [31:0] mem_access_addr,  
+    input [31:0] mem_write_data,  
     input mem_write_en,  
     input mem_read,  
 
-    output [15:0] mem_read_data  
+    output [31:0] mem_read_data  
  );  
       integer i;  
-      reg [15:0] ram [255:0];  // declares 256 element array at 16 bits wide each element
-      wire [7 : 0] ram_addr = mem_access_addr[8 : 1];  
+      reg [31:0] ram [255:0];  // declares 256 element array at 16 bits wide each element
+      wire [7 : 0] ram_addr = mem_access_addr[8 : 1];//wire   
       initial begin  
            for(i=0;i<256;i=i+1)  
-                ram[i] <= 16'd0;  //initialize ram array elements to 0 with size 16.
+                ram[i] <= 32'd0;  //initialize ram array elements to 0 with size 16.
       end  
       always @(posedge clk) begin  // runs if clock changes val from 0 -> 1 or 1 -> 0
            if (mem_write_en)        //if mem_write_en == 1 (in other words, is true)
                 ram[ram_addr] <= mem_write_data;  // changes ram[ram_addr] to mem_write_data when clock changes and mem_write_en == 1
       end  
       /** determines whether or not to write data to memory*/
-      assign mem_read_data = (mem_read==1'b1) ? ram[ram_addr]: 16'd0;   // Everytime mem_read changes, changes value of mem_read_data
+      assign mem_read_data = (mem_read==1'b1) ? ram[ram_addr]: 32'd0;   // Everytime mem_read changes, changes value of mem_read_data
  endmodule   
