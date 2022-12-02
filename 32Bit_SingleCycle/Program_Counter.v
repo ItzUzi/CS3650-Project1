e43d5154a8c44b67dcb432bb7840b39b72deb01a
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2022-12-01 17:58:58 -0800

    finishing up program counter
    adding comments

--- 32Bit_SingleCycle/Program_Counter.v
+++ 32Bit_SingleCycle/Program_Counter.v
@@ -9,12 +9,13 @@ input clk, rst_n;
 input [31:0] in_address;
 output reg [31:0] out_address; 
 
-always @ (posedge clk or negedge rst_n)
+//here the code will run whenever value of clk or rst_n change. 
+always @ (posedge clk or negedge rst_n) //posedge - 0 to 1 and negedge - 1 to 0
 begin
   if(!rst_n)
   out_address <= 32'd0; //initialize out_address with 0 with size 32.
   else
-  out_address <= in_address;
+  out_address <= in_address; //initialize out_address with in_address instead.
 end
 
 endmodule
