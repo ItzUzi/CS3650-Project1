dae0ddf70cfc9f5ccae5b57145ca7bef6fcef12e
andrew001347 <84065006+andrew001347@users.noreply.github.com>
2022-12-04 01:19:31 -0800

    Update Processor_Top.v

--- 32Bit_SingleCycle/Processor_Top.v
+++ 32Bit_SingleCycle/Processor_Top.v
@@ -25,7 +25,7 @@ wire [31:0] alu_result;
 wire zero_out;
 wire [31:0] datamem_read_data;
 //all of these contain a 32-bit width. Those that aren't assigned anything will have the default bit.
-assign addr_incr = (!rst_n) ? 32'd0 : 32'd4;
+assign addr_incr = (!rst_n) ? 32'd0 : 32'd4; //decimal 0 with 32 bit size : decimal 4 with 32 bit size
 assign final_write_en = (!rst_n) ? 1'b0 : ctrl_write_en;
 Program_Counter prg_cntr (
 .clk (clk),
